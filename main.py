234968271c65ce16844299687e7a4d1d0bcdf9b8
knine3 <kawishqayyum@gmail.com>
2018-05-29 01:28:50 +0500

    feat: count the files in iPhoto directory.

--- main.py
+++ main.py
@@ -11,9 +11,20 @@ path = "C:/Users/Kawish/Pictures/iPhotos"
 listA = []
 
 for (dirpath, dirnames, filenames) in os.walk(path):
-	listA.append((filenames))
+	listA.append((dirpath, filenames))
+
 
 print(len(listA))
+
+sum = int(0)
+
+for _ in listA:
+	print(_[0], ' : ', len(_[1]))
+	print()
+	sum += len(_[1])
+
+print('Total: ', sum)
+
 
 # print(listA[0][0])
 
