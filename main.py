3d79d6e1eb38604e623a46f483fa9d4aac08fe0f
knine3 <kawishqayyum@gmail.com>
2018-05-29 20:27:47 +0500

    refactor: removed unwanted code and data, i.e listA etc

--- main.py
+++ main.py
@@ -3,48 +3,14 @@ import os
 
 path = "C:/Users/Kawish/Pictures/iPhotos"
 
-# direc = os.listdir(path)
-
-# for _ in direc:
-# 	print(_)
-
 listA = []
+allFiles = ""
 
-for (dirpath, dirnames, filenames) in os.walk(path):
-	listA.append((dirpath, filenames))
+for path, subdirs, files in os.walk(path):
+	for file in files:
+		allFiles += file + " "
 
+duplicates = re.findall("\S* \(\d\)\S*", allFiles)
 
-# Total Files
-sum = int(0)
-
-for _ in listA:
-	print(_[0], ' : ', len(_[1]))
-	print()
-	sum += len(_[1])
-
-print('Total:', sum)
-
-
-# Finding Duplicates:
-
-PictureString = ''
-
-for _ in listA:
-	for nam in _[1]:
-		PictureString += nam + " "
-
-dups = re.findall ('\S*\s\(\d\)\S*', PictureString)
-
-for _ in dups:
-	print(_)
-
-# print(listA[0][0])
-
-# s = str("")
-# for _ in listA[0][1]:
-# 	s += _ + " "
-
-# pinges = re.findall ('\S*\(\d\)\S*', s)
-
-# print(pinges)
-
+print("Total Duplicates:", len(duplicates))
+print(duplicates)
