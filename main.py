4ae0e5fc39d94a4d9ba290dda887ee0ecdab19b9
knine3 <kawishqayyum@gmail.com>
2018-05-29 23:27:35 +0500

    feat: Calculate total size of duplicates

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ import os
 
 path = "C:/Users/Kawish/Pictures/iPhotos"
 
-listA = []
 allFiles = ""
 
 for path, subdirs, files in os.walk(path):
 	for file in files:
-		allFiles += file + " "
+		allFiles += os.path.join(path, file) + "\n"
 
-duplicates = re.findall("\S* \(\d\)\S*", allFiles)
+duplicates = re.findall(r"(\S* \(\d\)\S*)", allFiles)
 
-print("Total Duplicates:", len(duplicates))
-print(duplicates)
+print("Total Duplicate files:", len(duplicates))
+
+# Total Size
+totalSize = float(0)
+
+for _ in duplicates:
+	totalSize += os.path.getsize(_)
+
+totalSize = round(totalSize/(1048576*1024), 2)
+
+print("Total Size:", totalSize, "GB")
